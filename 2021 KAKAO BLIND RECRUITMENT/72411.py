f7e3253740419d409a425abc03cca7d5f263c554
s01va <jinn0525@gmail.com>
2021-09-05 23:00:13 +0900

    메뉴 리뉴얼

--- 2021 KAKAO BLIND RECRUITMENT/72411.py
+++ 2021 KAKAO BLIND RECRUITMENT/72411.py
@@ -1,33 +1,52 @@
 # -*- coding: utf-8 -*-
 
 # https://programmers.co.kr/learn/courses/30/lessons/72411
+from itertools import combinations
 import operator
-
-def countcomb(menulist, count):
 
 
 def solution(orders, course):
 	answer = []
-	menuStats = {}	# 메뉴 통계
-
-	for order in orders:
-		for menu in order:
-			if menuStats.get(menu):
-				menuStats[menu] += 1
-			else:
-				menuStats[menu] = 1
+	menu = []
 	
-	menuStats_sorted = sorted(menuStats.items(), key=operator.itemgetter(1), reverse=True)
-	print(menuStats_sorted)
+	for order in orders:
+		for m in order:
+			if m in menu:
+				pass
+			else:
+				menu.append(m)
+	
+	menu.sort()
+	
+	for c in course:
+		comb_stats = {}
+		combs = list(combinations(menu, c)) # 코스 메뉴 수만큼의 조합
+		for comb in combs:
+			comb_stats[comb] = 0
+			for order in orders:
+				ex_flag = False
+				for i in comb:
+					if i in order:
+						ex_flag = True
+					else:
+						ex_flag = False
+						break
+				if ex_flag == True:
+					comb_stats[comb] += 1
 
-	for count in course:
-		countcomb(menuStats_sorted, count)
-
-
+		comb_stats = sorted(comb_stats.items(), key=operator.itemgetter(1), reverse=True)
+		comb_max = comb_stats[0][1]
+		if comb_max >= 2:
+			for comb in comb_stats:
+				if comb[1] != comb_max:
+					break
+				answer.append(''.join(comb[0]))
+	
+	answer.sort()
 	return answer
 
-orders, course = ["ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"], [2,3,4]
-#orders, course = ["ABCDE", "AB", "CD", "ADE", "XYZ", "XYZ", "ACD"], course = [2,3,5]
+#orders, course = ["ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"], [2,3,4]
+orders, course = ["ABCDE", "AB", "CD", "ADE", "XYZ", "XYZ", "ACD"], [2,3,5]
 #orders, course = ["XYZ", "XWY", "WXA"], [2,3,4]	
 
 solution(orders, course)
