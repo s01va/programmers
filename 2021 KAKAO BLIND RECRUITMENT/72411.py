749d3ccd8448c31808f8ae5484581f7193e56389
s01va <jinn0525@gmail.com>
2021-09-09 17:52:35 +0900

    메뉴 리뉴얼

--- 2021 KAKAO BLIND RECRUITMENT/72411.py
+++ 2021 KAKAO BLIND RECRUITMENT/72411.py
@@ -2,51 +2,34 @@
 
 # https://programmers.co.kr/learn/courses/30/lessons/72411
 from itertools import combinations
-import operator
+from collections import Counter
 
 
 def solution(orders, course):
 	answer = []
-	menu = []
-	
-	for order in orders:
-		for m in order:
-			if m in menu:
-				pass
-			else:
-				menu.append(m)
-	
-	menu.sort()
-	
-	for c in course:
-		comb_stats = {}
-		combs = list(combinations(menu, c)) # 코스 메뉴 수만큼의 조합
-		for comb in combs:
-			comb_stats[comb] = 0
-			for order in orders:
-				ex_flag = False
-				for i in comb:
-					if i in order:
-						ex_flag = True
-					else:
-						ex_flag = False
-						break
-				if ex_flag == True:
-					comb_stats[comb] += 1
 
-		comb_stats = sorted(comb_stats.items(), key=operator.itemgetter(1), reverse=True)
-		comb_max = comb_stats[0][1]
-		if comb_max >= 2:
-			for comb in comb_stats:
-				if comb[1] != comb_max:
+	for c in course:
+		allcomb = []
+		for order in orders:
+			comb = combinations(sorted(list(order)), c)
+			allcomb += comb
+		
+		if len(allcomb) == 0:
+			continue
+		counter = Counter(allcomb).most_common()
+		maxc = counter[0][1]
+		if maxc >= 2:
+			answer.append(''.join(counter[0][0]))
+			for i in range(1, len(counter)):
+				if counter[i][1] != maxc:
 					break
-				answer.append(''.join(comb[0]))
+				answer.append(''.join(counter[i][0]))
 	
 	answer.sort()
 	return answer
 
 #orders, course = ["ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"], [2,3,4]
-orders, course = ["ABCDE", "AB", "CD", "ADE", "XYZ", "XYZ", "ACD"], [2,3,5]
-#orders, course = ["XYZ", "XWY", "WXA"], [2,3,4]	
+#orders, course = ["ABCDE", "AB", "CD", "ADE", "XYZ", "XYZ", "ACD"], [2,3,5]
+orders, course = ["XYZ", "XWY", "WXA"], [2,3,4]	
 
 solution(orders, course)
